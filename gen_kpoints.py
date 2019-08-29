b44287875cd22250318f82ddba572e71749bff54
koktszfung <koktszfung@gmail.com>
2019-08-29 16:24:34 +0800

    cleaned up gen_kpoints.py

--- gen_kpoints.py
+++ gen_kpoints.py
@@ -1,11 +1,35 @@
-half_size = 8
-full_size = half_size*2+1
-scale = 1/half_size
-with open("vasp_inputs/KPOINTS", "w") as file:
-    file.write(f"KPOINTS {full_size}x{full_size}x{full_size}\n")
-    file.write(str(full_size**3)+"\n")
-    file.write("C\n")
-    for i in range(-half_size, half_size+1):
-        for j in range(-half_size, half_size+1):
-            for k in range(-half_size, half_size+1):
-                file.write(f"{i*scale:.5} {j*scale:.5} {k*scale:.5} 1.\n")
+import numpy
+import data
+
+
+def write_kpoints_cubic(half_size: int, coord_style: chr, out_file_path):
+    full_size = half_size * 2 + 1
+    scale = 1 / half_size
+    with open(out_file_path, "w") as file:
+        file.write(f"KPOINTS cubic {half_size}x{half_size}x{half_size}\n")
+        file.write(str(full_size ** 3) + "\n")
+        file.write(f"{coord_style}\n")
+        for i in range(-half_size, half_size + 1):
+            for j in range(-half_size, half_size + 1):
+                for k in range(-half_size, half_size + 1):
+                    file.write(f"{i*scale:.5} {j*scale:.5} {k*scale:.5} 1.\n")
+
+
+def write_kpoints_from_file(in_file_path: str, out_file_path: str):
+    vasprun_root = data.get_vasprun_root(in_file_path)
+    kpoints = data.get_kpoints(vasprun_root)
+    with open(out_file_path, "w") as file:
+        file.write(f"KPOINTS {in_file_path}\n")
+        file.write(str(kpoints.shape[0]) + "\n")
+        file.write("R\n")
+        for kpoint in kpoints:
+            file.write(f"{kpoint[0]:.5} {kpoint[1]:.5} {kpoint[2]:.5} 1.\n")
+
+
+def main():
+    # write_kpoints_cubic(4, 'R', "vasp_inputs/KPOINTS")
+    write_kpoints_from_file("vasp_outputs/sg1/696736_c.xml", "vasp_inputs/KPOINTS")
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import plot
 if __name__ == '__main__':
     p = plot.Plotter()
 
-    axis, layer = 2, 4
+    axis = 1
 
-    r = data.Reader("vasp_outputs/sg1/696736_c.xml")
-    band_indices = data.get_band_indicecs(r.orbitals, lambda x: x <= 1)[-1:]
-    p.add_bands_surface_plot(r.kpoints_reciprocal, r.eigenvalues, axis, layer, 27, band_indices)
-    p.add_bands_scatter_plot(r.kpoints_reciprocal, r.eigenvalues, axis, layer, band_indices)
-
+    layer = 8
     r = data.Reader("vasp_outputs/sg1/696736_c_17.xml")
-    band_indices = data.get_band_indicecs(r.orbitals, lambda x: x <= 1)[-1:]
-    p.add_bands_surface_plot(r.kpoints_reciprocal, r.eigenvalues, axis, layer, 51, band_indices,
-                             [2.5, 0, 0])
-    p.add_bands_scatter_plot(r.kpoints_reciprocal, r.eigenvalues, axis, layer, band_indices,
-                             [2.5, 0, 0])
+    band_indices = data.get_band_indicecs(r.orbitals, lambda x: x == 2)[-1:]
+    p.add_bands_surface_plot(r.kpoints, r.eigenvalues, axis, layer, 64, band_indices)
+    # p.add_bands_scatter_plot(r.kpoints, r.eigenvalues, axis, layer, band_indices)
+    # p.add_kpoints_scatter_plot(r.kpoints, axis, layer)
+
+    layer = 4
+    r = data.Reader("vasp_outputs/sg1/696736_c.xml")
+    band_indices = data.get_band_indicecs(r.orbitals, lambda x: x == 2)[-1:]
+    p.add_bands_surface_plot(r.kpoints, r.eigenvalues, axis, layer, 64, band_indices, [15, 0, 0])
+    # p.add_bands_scatter_plot(r.kpoints, r.eigenvalues, axis, layer, band_indices, [0, 0, 0])
+    # p.add_kpoints_scatter_plot(r.kpoints, axis, layer, [15, 0, 0])
 
     p.show()
